34d08e730346e083ed322a817fd4d6b7fcc3fae0
Ubuntu <ubuntu@ip-172-31-4-168.ap-south-1.compute.internal>
2026-01-09 07:45:44 +0000

    First

---
+++ Basic/hello.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This is script for me
+
+echo "Trying out different stuff"
+
---
+++ Basic/var.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This is a comment that doesn't not included which running the script
+
+<< comment
+Multi
+line comment
+by using the same name at the start and the end
+comment
+
+name="Alok"
+
+echo "Name is $name , date is $(date)"
+
+echo "Enter the name:"
+
+read username
+
+echo "You entered $username"
+
+echo "Printing the arugments , which is given which executing the file
+The First arug is $1 and the second arug is $2 of the file $0"
